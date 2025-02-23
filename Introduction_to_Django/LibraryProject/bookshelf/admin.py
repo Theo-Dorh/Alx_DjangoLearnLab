402b7bc46c28893cd4f77ce000cfe7be6bc8589e
Theo-Dorh <dorhtheophilus@gmail.com>
2025-02-23 21:01:18 +0000

    Combined book model with admin interface

--- Introduction_to_Django/LibraryProject/bookshelf/admin.py
+++ Introduction_to_Django/LibraryProject/bookshelf/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
-# Register your models here.
+from django.contrib import admin
+from .models import Book
+
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'publication_year')
+    list_filter = ('author', 'publication_year')
+    search_fields = ('title', 'author')
+
+# Unregister first to avoid AlreadyRegistered error
+# admin.site.unregister(Book)  # Add this line
+admin.site.register(Book, BookAdmin)
+
